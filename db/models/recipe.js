513b57ecb0923eaaa346bbd44d9e1fc40b31afb5
Nathan <nathanmac777@hotmail.com>
2021-05-03 16:24:19 -0700

    recipe and recipe box migrations and models

--- db/models/recipe.js
+++ db/models/recipe.js
@@ -1,11 +1,26 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Recipe = sequelize.define('Recipe', {
-    name: DataTypes.STRING,
-    userId: DataTypes.INTEGER
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {});
-  Recipe.associate = function(models) {
-    // associations can be defined here
+  Recipe.associate = function (models) {
+    Recipe.belongsTo(models.User, { foreignKey: "userId" });
+    Recipe.hasMany(models.Ingredient, { foreignKey: "recipeId" });
+    Recipe.hasMany(models.Instruction, { foreignKey: "recipeId" });
+    Recipe.hasMany(models.Review, { foreignKey: "recipeId" });
+    Recipe.hasMany(models.Like, { foreignKey: "recipeId" });
+    Recipe.belongsToMany(models.RecipeBox, {
+      through: "RecipeBoxJoinTable",
+      otherKey: "recipeBoxId",
+      foreignKey: "recipeId"
+    })
   };
   return Recipe;
 };
