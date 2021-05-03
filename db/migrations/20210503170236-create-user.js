cd681f3ed7cf379c6daff6fe28da2ce39d35627e
Bill Adams <vikingbill@gmail.com>
2021-05-03 13:10:24 -0400

    create migration for users

---
+++ db/migrations/20210503170236-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING(35),
+        unique: true,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        // check to see if email is a datatype, if so update migration
+        unique: true
+      },
+      hashedPassword: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
