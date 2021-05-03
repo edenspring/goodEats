f8d1a912f84d134951fd5f471716c31fdc498f94
Melonnie <melonnie13@gmail.com>
2021-05-03 17:15:17 -0500

    updated model constraints

--- db/models/user.js
+++ db/models/user.js
@@ -1,9 +1,20 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    hashedPassword: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING(35),
+      allowNull: false,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false
+    }
   }, {});
   User.associate = function(models) {
     // associations can be defined here
