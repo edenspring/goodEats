f9d3813c6f698702c3f3359df24bde4e2472825a
Bill Adams <vikingbill@gmail.com>
2021-05-03 14:11:56 -0400

    added skeleton for user routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,14 @@
 var express = require('express');
+const csrf = require('csurf');
+const bcrypt = require('bcryptjs')
+
+const db = require('../db/models')
+const csrfProtection = csrf({ cookie: true });
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
 var router = express.Router();
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,5 +18,18 @@ router.get('/', function(req, res, next) {
 router.get('/login', (req, res) => {
   res.render('users-login');
 });
+
+router.get('/register', (req, res) => {
+  res.render('users-register', {
+    title: "Register a new User",
+    user,
+    csrfToken: req.csrfToken(),
+  });
+})
+
+router.post('/', asyncHandler(async(req, res, next)=>{
+  const {userName, email, password, confirmedPassworrd} = req.body;
+  
+}))
 
 module.exports = router;
