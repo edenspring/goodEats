2b061d9703215e8f00396ebb0850df55782b8bdf
Nathan <nathanmac777@hotmail.com>
2021-05-05 16:32:04 -0700

    box add recipes/remove recipes/delete box, delete recipe confirmations

--- routes/recipes.js
+++ routes/recipes.js
@@ -128,4 +128,43 @@ router.get("/:id/edit", asyncHandler(async (req, res, next) => {
     }
 }))
 
+router.get("/:id/delete", asyncHandler(async (req, res) => {
+    const recipeId = parseInt(req.params.id, 10);
+    const userId = req.session.auth.userId;
+    const recipe = await Recipe.findByPk(recipeId);
+    checkPermissions(recipe, userId);
+    res.render('recipe-delete-confirm', { recipeId, recipe})
+}))
+
+router.post("/:id/delete", asyncHandler(async (req, res, next) => {
+    const recipeId = parseInt(req.body.recipeId, 10);
+    const userId = req.session.auth.userId;
+    const recipe = await Recipe.findByPk(recipeId);
+    if (recipe) {
+        const ingredients = await Ingredient.findAll({
+            where: {
+                recipeId: recipeId
+            },
+        });
+        const instructions = await Instruction.findAll({
+            where: {
+                recipeId: recipeId
+            },
+            order: [
+                ['listOrder', 'ASC']
+            ]
+        });
+        instructions.forEach(async (instruction) => {
+            await instruction.destroy();
+        })
+        ingredients.forEach(async (ingredient) => {
+            await ingredient.destroy();
+        })
+        await recipe.destroy();
+        res.redirect("/");
+    } else {
+        next(recipeNotFoundError(recipeId));
+    }
+}))
+
 module.exports = router;
