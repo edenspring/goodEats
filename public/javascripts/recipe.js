da80ff45d4aee558b8c29217f390ef32054eac48
Bill Adams <vikingbill@gmail.com>
2021-05-05 14:15:47 -0400

    lunch time commit

--- public/javascripts/recipe.js
+++ public/javascripts/recipe.js
@@ -1,5 +1,13 @@
 window.addEventListener("load", (event)=>{
-  document.querySelector('.reviews__button').addEventListener('click', (e)=>{
-    
+  const reviewButton = document.querySelector('.review__button');
+  reviewButton.addEventListener('click', (e)=>{
+    const reviewForm = document.querySelector('.reviews__form');
+    if (reviewForm.style.display === "none"){
+      reviewForm.style.display = "block";
+      reviewButton.innerText = 'Cancel writing review'
+    } else {
+      reviewForm.style.display = "none";
+      reviewButton.innerText = 'Write a review'
+    }
   })
 })
