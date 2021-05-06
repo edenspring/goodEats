353d53ceb8f54dcd4f327ecd4747f5c3b604166d
Nathan <nathanmac777@hotmail.com>
2021-05-06 11:22:55 -0700

    navbar, home page css

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { asyncHandler } = require('./utils');
-const { Recipe } = require("../db/models");
+const { Recipe, Picture } = require("../db/models");
 
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res) => {
   const recipes = await Recipe.findAll({
+    include: { model: Picture },
     order: [
       ["updatedAt", "DESC"]
     ]
   });
+  console.log(recipes[0].Pictures[0].dataValues.src);
   res.render('home', { title: "GoodEats", recipes });
 }));
 
