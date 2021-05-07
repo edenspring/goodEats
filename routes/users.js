45f9f9840db56e100b539c25b2c99036ff8641e8
Nathan <nathanmac777@hotmail.com>
2021-05-07 09:41:03 -0700

    change user register to redirect to home page, clean up unused files

--- routes/users.js
+++ routes/users.js
@@ -16,12 +16,20 @@ const logInValidator = [
 ];
 
 /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.render("home", { title: "Welcome", user });
-// });
+router.get('/', function (req, res, next) {
+  if (req.session.auth) {
+    res.redirect("/");
+  } else {
+    res.redirect("/users/login");
+  }
+});
 
 router.get('/login', csrfProtection, (req, res) => {
-  res.render('users-login', { title: "Log In", csrfToken: req.csrfToken() });
+  if (req.session.auth) {
+    res.redirect("/");
+  } else {
+    res.render('users-login', { title: "Log In", csrfToken: req.csrfToken() });
+  }
 });
 
 router.post('/login', csrfProtection, logInValidator, asyncHandler( async(req, res) => {
@@ -68,12 +76,16 @@ router.get('/logout', (req, res) => {
 });
 
 router.get('/register', csrfProtection, (req, res) => {
-  const user = db.User.build();
-  res.render('users-register', {
-    title: "Register a new User",
-    user,
-    csrfToken: req.csrfToken(),
-  });
+  if (req.session.auth) {
+    res.redirect("/");
+  } else {
+    const user = db.User.build();
+    res.render('users-register', {
+      title: "Register a new User",
+      user,
+      csrfToken: req.csrfToken(),
+    });
+  }
 })
 
 
@@ -144,8 +156,8 @@ router.post('/register', csrfProtection, registerUserValidators, asyncHandler(as
     const hashedPassword = await bcrypt.hash(password, 10);
     user.hashedPassword = hashedPassword;
     await user.save();
-    loginUser(req, res, user)
-    res.render('index')
+    loginUser(req, res, user);
+    res.redirect("/");
   }
   else {
     const errors = validatorErrors.array().map((e)=>e.msg);
