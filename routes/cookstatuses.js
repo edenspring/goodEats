9b5d76878a23206ee8d53cac6df512c2c05158a7
Bill Adams <vikingbill@gmail.com>
2021-05-07 18:19:23 -0400

    in progress comments

--- routes/cookstatuses.js
+++ routes/cookstatuses.js
@@ -1,37 +1,71 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { asyncHandler } = require('./utils');
-const { check, validationResult } = require('express-validator');
-const { Ingredient, Instruction, Recipe, Review, Picture, Like, CookStatus } = require('../db/models');
-const { loginUser, logoutUser, requireAuth, restoreUser, checkPermissions } = require('../auth')
-const Sequelize = require("sequelize");
-const Pictures = require('../db/seeders/8-Pictures');
+const { asyncHandler } = require("./utils");
+const { check, validationResult } = require("express-validator");
+const { CookStatus } = require("../db/models");
+const { checkPermissions } = require("../auth");
+const { noExtendLeft } = require("sequelize/types/lib/operators");
 
-router.post('/', asyncHandler(async(req, res)=>{
-  const {userId, recipeId, cookStatus} = req.body;
-  console.log('user :', userId)
-  console.log('recipe :', recipeId)
-  console.log('status :', cookStatus)
-  const workingStatus = await CookStatus.findOne({
-    where: {
-      userId: userId,
-      recipeId: recipeId
+//define constraints for status 
+const statusValidator = [
+  check("userId")
+    .exists({ checkFalsy: true })
+    .withMessage("current userId must be provided"),
+  check("recipeId")
+    .exists({ checkFalsy: true })
+    .withMessage("current recipeId must be provided"),
+  check("status")
+    .exists({ checkFalsy: true })
+    .withMessage("a valid status must be provided"),
+];
+
+router.post(
+  "/",
+  statusValidator,
+  asyncHandler(async (req, res, next) => {
+    const { userId, recipeId, cookStatus } = req.body;
+
+    //check for validation errors
+    const validatorErrors = validationResult(req);
+    if (validatorErrors.isEmpty()) {
+
+      //attempt to find User's status for recipe, if it exists
+      const workingStatus = await CookStatus.findOne({
+        where: {
+          userId: userId,
+          recipeId: recipeId,
+        },
+      });
+
+      //find current authenticated user from
+      const currentUser = res.locals.user.id;
+      if (workingStatus) {
+        //verify current user is authorized to make changes
+        checkPermissions(workingStatus, currentUser);
+
+        //update status
+        workingStatus.status = cookStatus;
+        await workingStatus.save();
+        res.sendStatus(200).end;
+      } else {
+        //if no status found for user and recipe, build new
+        const newStatus = CookStatus.build({
+          userId,
+          recipeId,
+          status: cookStatus,
+        });
+        //verify current user is the one making request
+        checkPermissions(workingStatus, newStatus);
+
+        //save review
+        await newStatus.save();
+        res.sendStatus(200).end();
+      }
+    } else {
+      const errors = validatorErrors.array().map((e) => e.msg);
+      next(errors);
     }
   })
-  console.log(workingStatus)
-  if (workingStatus){
-    workingStatus.status = cookStatus;
-    await workingStatus.save();
-    res.sendStatus(200).end;
-  } else {
-      const newStatus = CookStatus.build({
-      userId,
-      recipeId,
-      status: cookStatus
-    })
-    await newStatus.save();
-    res.sendStatus(200).end();
-  }
-}))
+);
 
 module.exports = router;
