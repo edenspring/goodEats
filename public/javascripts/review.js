b2f91f251c141183d6dd886f9dc568e18f9c2899
Bill Adams <vikingbill@gmail.com>
2021-05-09 22:35:54 -0400

    add missing functionality, resolve issues

--- public/javascripts/review.js
+++ public/javascripts/review.js
@@ -3,24 +3,31 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   editButton.addEventListener("click", (e) => {
     e.preventDefault();
-
+    console.log("a new thing");
     const editDiv = document.createElement("div");
     editDiv.classList.add("review-edit-container");
     const editTextArea = document.createElement("textarea");
     editTextArea.classList.add("review-edit-text-area");
     const confirmButton = document.createElement("button");
     confirmButton.classList.add("confirm-button", "btn");
+    confirmButton.innerHTML = "Confirm Changes";
 
     editDiv.appendChild(editTextArea);
     editDiv.appendChild(confirmButton);
+
+    editButton.parentElement.appendChild(editDiv);
 
     confirmButton.addEventListener("click", (e) => {
       e.preventDefault();
 
       const reviewId = document.getElementById("reviewIdForReviewEdit").value;
-      const reviewContent = document.getElementById("review-content").value;
+      const reviewContent = editTextArea.value;
 
       editReview(reviewId, reviewContent);
+      const reviewsDiv = editButton.closest(".reviews");
+      console.log(reviewsDiv);
+      reviewsDiv.firstChild.children[1].innerHTML = reviewContent;
+      editDiv.remove();
     });
   });
 
@@ -28,7 +35,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     data = {};
     data.reviewId = reviewId;
     data.review = review;
-    // console.log(data);F
+    // console.log(data);
     await fetch(`/reviews/${reviewId}/edit`, {
       method: "PATCH",
       headers: {
